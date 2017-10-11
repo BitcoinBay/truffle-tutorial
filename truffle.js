cbab643018094a64a12327223bffbfed3fde380e
Subramanian Venkatesan <subramanian.bsv@gmail.com>
2017-10-11 16:34:44 -0400

    Truffle setup

--- truffle.js
+++ truffle.js
@@ -1,9 +1,21 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "REPLACE WITH YOUR MNEMOIC";
+
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
-    }
+    },
+    ropsten: {
+     provider: new HDWalletProvider(mnemonic, "https://testnet.infura.io/"),
+     network_id: 3
+   },
+   rinkeby : {
+     provider: new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/"),
+     network_id : 4
+   }
   }
 };
+
